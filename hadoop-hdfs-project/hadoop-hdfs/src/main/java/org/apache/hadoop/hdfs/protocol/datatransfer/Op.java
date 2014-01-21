d35d2588c35dcfcdc4d4c72ba2605a1c908de29b
arrowli <lizhenqiangsnake@gmail.com>
2014-01-21 12:14:12 +0800

    patch

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocol/datatransfer/Op.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocol/datatransfer/Op.java
@@ -40,7 +40,8 @@ public enum Op {
   RSYNC_CHUNKS_CHECKSUM((byte)88),
   RSYNC_INFLATE_BLOCK((byte)89),
   RSYNC_CALCULATE_SEGMENTS((byte)90),
-  RSYNC_SEND_SEGMENT((byte)91);
+  RSYNC_SEND_SEGMENT((byte)91),
+  RSYNC_UPDATE_BLOCK((byte)92);
 
   /** The code for this operation. */
   public final byte code;
